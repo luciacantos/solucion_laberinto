122474af31599c2b83438aa118005a04a72f7414
luciacantos <luciacantos22@gmail.com>
2022-12-08 21:49:39 +0100

    movimiento 'Derecha'

--- laberinto_final.py
+++ laberinto_final.py
@@ -29,10 +29,16 @@ def laberinto_final(laberinto):
     #seguira moviéndose hasta encontrar la salida ('S')
     while ( fila <= 5 and columna <= 5) and laberinto[fila][columna] != 'S': #5 es la longitud del laberinto tanto para sus filas y sus columanas ya que es un cuadrado
        #-----ABAJO--------------------------------------------------------
-        # la fila en la que estás más el siguiente paso que darías para abajo no puede ser mayor que el número de filas del laberinto (5)
-        # es necesario no tocar los muros por lo que tiene que haber '' en la siguiente casilla( definido como espacio en la amterior práctica del dibujo del laberinto)
-        # no se puede volver a la casilla anterior por lo que hay que comprobar en la lista de movimientos que  el moviemto anterior no era hacia arriba
+        #la casilla inferior de la que estás no puede ser mayor que el número de filas del laberinto (5)
+        #es necesario no tocar los muros por lo que tiene que haber '' en la siguiente casilla (definido como espacio en la amterior práctica del dibujo del laberinto)
+        #no se puede volver a la casilla anterior por lo que hay que comprobar en la lista de movimientos que  el moviemto anterior no era hacia arriba
         if fila + 1 <= 5 and laberinto[fila + 1][columna] == '' and solucion[-1] != 'Arriba':
             solucion.append('Abajo')
             fila = fila + 1
-        
+        #-----DERECHA------------------------------------------------------
+        #la casilla derecha de la que estás no puede ser mayor que el número de columnas del laberinto (5)
+        #no puedes tocar un muro por lo que tiene que haber '' en la siguiente casilla
+        #no se puede volver a la casilla anterior por lo que compruebas en la lista que el movimiento anterior no sea 'Izquierda'
+        if columna + 1 <= 5 and laberinto[fila][columna+1] == '' and solucion[-1] != 'Izquierda':
+            solucion. append('Derecha')
+            columna = columna + 1
