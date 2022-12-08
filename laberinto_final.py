0771293b9c05b3554f493b1470fb36cac278786e
luciacantos <luciacantos22@gmail.com>
2022-12-08 22:02:04 +0100

    movimiento 'Arriba'

--- laberinto_final.py
+++ laberinto_final.py
@@ -25,13 +25,13 @@ def laberinto_final(laberinto):
     fila = 0
     #inicializar lista para guardar los movimientos
     solucion = ['Inicio']
-    #bucle para que mientras que no se salga del laberinto ni llegue a la salida'S' 
+    #bucle para que mientras que no se salga del laberinto ni llegue a la salida'S'
     #seguira moviéndose hasta encontrar la salida ('S')
     while ( fila <= 5 and columna <= 5) and laberinto[fila][columna] != 'S': #5 es la longitud del laberinto tanto para sus filas y sus columanas ya que es un cuadrado
-       #-----ABAJO--------------------------------------------------------
+    #-----ABAJO--------------------------------------------------------
         #la casilla inferior de la que estás no puede ser mayor que el número de filas del laberinto (5)
-        #es necesario no tocar los muros por lo que tiene que haber '' en la siguiente casilla (definido como espacio en la amterior práctica del dibujo del laberinto)
-        #no se puede volver a la casilla anterior por lo que hay que comprobar en la lista de movimientos que  el moviemto anterior no era hacia arriba
+        #la casilla tiene que tener '' para no tocar un muro  (definido como espacio en la amterior práctica del dibujo del laberinto)
+        #para no volver a la casilla anterior por lo que hay que comprobar en la lista de movimientos que  el moviemto anterior no era hacia arriba
         if fila + 1 <= 5 and laberinto[fila + 1][columna] == '' and solucion[-1] != 'Arriba':
             solucion.append('Abajo')
             fila = fila + 1
@@ -39,6 +39,15 @@ def laberinto_final(laberinto):
         #la casilla derecha de la que estás no puede ser mayor que el número de columnas del laberinto (5)
         #no puedes tocar un muro por lo que tiene que haber '' en la siguiente casilla
         #no se puede volver a la casilla anterior por lo que compruebas en la lista que el movimiento anterior no sea 'Izquierda'
-        if columna + 1 <= 5 and laberinto[fila][columna+1] == '' and solucion[-1] != 'Izquierda':
+        elif columna + 1 <= 5 and laberinto[fila][columna+1] == '' and solucion[-1] != 'Izquierda':
             solucion. append('Derecha')
             columna = columna + 1
+        #-----ARRIBA----------------------------------------------------
+        #la casilla superior de la que estás no puede ser inferior a 0 o estarías fuera del laberinto
+        #la casilla tiene que tener '' para no tocar un muro
+        #para no volver a la casilla anterior compruebas en la listade movimientos que el arterior no es ' Abajo'
+        elif fila - 1 >= 0 and laberinto[fila - 1][columna] == '' and solucion[-1] != 'Abajo'
+            solucion.append('Arriba')
+            fila = fila - 1
+        
+
