e93df332472ccce58cbaaea93a42a9189d64f553
luciacantos <luciacantos22@gmail.com>
2022-12-08 21:41:52 +0100

    moviento 'Abajo'

--- laberinto_final.py
+++ laberinto_final.py
@@ -28,3 +28,11 @@ def laberinto_final(laberinto):
     #bucle para que mientras que no se salga del laberinto ni llegue a la salida'S' 
     #seguira moviéndose hasta encontrar la salida ('S')
     while ( fila <= 5 and columna <= 5) and laberinto[fila][columna] != 'S': #5 es la longitud del laberinto tanto para sus filas y sus columanas ya que es un cuadrado
+       #-----ABAJO--------------------------------------------------------
+        # la fila en la que estás más el siguiente paso que darías para abajo no puede ser mayor que el número de filas del laberinto (5)
+        # es necesario no tocar los muros por lo que tiene que haber '' en la siguiente casilla( definido como espacio en la amterior práctica del dibujo del laberinto)
+        # no se puede volver a la casilla anterior por lo que hay que comprobar en la lista de movimientos que  el moviemto anterior no era hacia arriba
+        if fila + 1 <= 5 and laberinto[fila + 1][columna] == '' and solucion[-1] != 'Arriba':
+            solucion.append('Abajo')
+            fila = fila + 1
+        
